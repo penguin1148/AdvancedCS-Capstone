05ccffb5465a13385f4f23e1c35627a804215d13
MyCodeSMH <114875099+MyCodeSMH@users.noreply.github.com>
2026-04-28 09:58:31 -0700

    Create trustedSources.js with news sources by country
    Added a list of trusted news sources categorized by country.

---
+++ trustedsources.js
@@ -0,0 +1,164 @@
+// Top 5 reliable / reputable news sources by country
+// Use this in script.js
+
+const trustedSources = {
+  US: [
+    "Reuters",
+    "Associated Press",
+    "NPR",
+    "PBS NewsHour",
+    "Wall Street Journal"
+  ],
+
+  IN: [
+    "The Hindu",
+    "Indian Express",
+    "Reuters",
+    "BBC News",
+    "NDTV"
+  ],
+
+  GB: [
+    "BBC News",
+    "Reuters",
+    "Financial Times",
+    "The Guardian",
+    "Sky News"
+  ],
+
+  CA: [
+    "CBC News",
+    "Reuters",
+    "The Globe and Mail",
+    "CTV News",
+    "National Post"
+  ],
+
+  AU: [
+    "ABC News",
+    "Reuters",
+    "The Sydney Morning Herald",
+    "SBS News",
+    "The Australian"
+  ],
+
+  FR: [
+    "France 24",
+    "Reuters",
+    "Le Monde",
+    "AFP",
+    "Euronews"
+  ],
+
+  DE: [
+    "DW",
+    "Reuters",
+    "Der Spiegel",
+    "Frankfurter Allgemeine",
+    "Tagesschau"
+  ],
+
+  JP: [
+    "NHK",
+    "Reuters",
+    "Japan Times",
+    "Kyodo News",
+    "Nikkei Asia"
+  ],
+
+  BR: [
+    "Reuters",
+    "Folha de S.Paulo",
+    "O Globo",
+    "Estadão",
+    "UOL Notícias"
+  ],
+
+  MX: [
+    "Reuters",
+    "El Universal",
+    "Milenio",
+    "La Jornada",
+    "Animal Político"
+  ],
+
+  ES: [
+    "Reuters",
+    "El País",
+    "RTVE",
+    "ABC",
+    "La Vanguardia"
+  ],
+
+  IT: [
+    "Reuters",
+    "ANSA",
+    "Corriere della Sera",
+    "La Repubblica",
+    "RAI News"
+  ],
+
+  KR: [
+    "Yonhap News",
+    "Reuters",
+    "Korea Herald",
+    "KBS News",
+    "JoongAng Daily"
+  ],
+
+  CN: [
+    "Reuters",
+    "Caixin",
+    "South China Morning Post",
+    "Xinhua",
+    "China Daily"
+  ],
+
+  RU: [
+    "Reuters",
+    "Meduza",
+    "TASS",
+    "Novaya Gazeta",
+    "The Moscow Times"
+  ],
+
+  ZA: [
+    "Reuters",
+    "News24",
+    "Mail & Guardian",
+    "Business Day",
+    "SABC News"
+  ],
+
+  NG: [
+    "Reuters",
+    "Punch",
+    "Premium Times",
+    "Channels TV",
+    "The Guardian Nigeria"
+  ],
+
+  PK: [
+    "Reuters",
+    "Dawn",
+    "The News International",
+    "Geo News",
+    "Express Tribune"
+  ],
+
+  SG: [
+    "Reuters",
+    "The Straits Times",
+    "Channel NewsAsia",
+    "Today Online",
+    "Business Times"
+  ],
+
+  AE: [
+    "Reuters",
+    "The National",
+    "Gulf News",
+    "Khaleej Times",
+    "Arab News"
+  ]
+};
